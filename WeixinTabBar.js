ccb6647d3d9b0c660f593223ea198ec9ad81aa65
iamsongqian <iamsongqian@qq.com>
2018-05-01 20:39:53 +0800

    1

--- WeixinTabBar.js
+++ WeixinTabBar.js
@@ -5,17 +5,23 @@ import {
 	StyleSheet,
 	View,
 	TouchableOpacity,
-	Text
+	Text,
+	Image
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
+import news from './img/new.png';
+import cart from './img/cart.png';
+import account from './img/account.png';
 
+const tab=['资讯','商城','我的']
 const styles = StyleSheet.create({
 	tabs: {
 		flexDirection: 'row',
 		justifyContent:'space-around',
 		borderTopColor: '#F0F0F0',
 		borderTopWidth: 0.5,
+		paddingVertical: 5,
 	},
 
 	tab: {
@@ -30,30 +36,38 @@ const styles = StyleSheet.create({
 });
 export default class WeixinTabBar extends Component {
 	constructor(){
-		super()
+		super();
+		this.state={
+			tab:0
+		}
 	}
-
-	renderTabOption(tab, i) {
-		let color = this.props.activeTab == i ? "#F2180D" : "#ADADAD"; // 判断i是否是当前选中的tab，设置不同的颜色
-		return (
-			<TouchableOpacity onPress={()=>this.props.goToPage(i)} style={styles.tab} key={i}>
-				<View style={styles.tabItem}>
-					<Icon
-						name={this.props.tabIconNames[i]} // 图标
-						size={30}
-						color={color}/>
-					<Text style={{color: color}}>
-						{this.props.tabNames[i]}
-					</Text>
-				</View>
-			</TouchableOpacity>
-		);
-	}
-
 	render() {
 		return (
 			<View style={styles.tabs}>
-				{this.props.tabs.map((tab, i) => this.renderTabOption(tab, i))}
+				<TouchableOpacity onPress={()=>{this.props.goToPage(0);this.setState({tab:0})}}>
+					<View style={styles.tabItem}>
+						<Image source={news} style={{width: 20, height: 20}}/>
+						<Text style={{color: this.state.tab===0?'#F2180D':'#ADADAD'}}>
+							{tab[0]}
+						</Text>
+					</View>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={()=>{this.props.goToPage(0);this.setState({tab:1})}}>
+					<View style={styles.tabItem}>
+						<Image source={cart} style={{width: 20, height: 20}}/>
+						<Text style={{color: this.state.tab===1?'#F2180D':'#ADADAD'}}>
+							{tab[1]}
+						</Text>
+					</View>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={()=>{this.props.goToPage(0);this.setState({tab:2})}}>
+					<View style={styles.tabItem}>
+						<Image source={account} style={{width: 20, height: 20}}/>
+						<Text style={{color: this.state.tab===2?'#F2180D':'#ADADAD'}}>
+							{tab[2]}
+						</Text>
+					</View>
+				</TouchableOpacity>
 			</View>
 		);
 	}
